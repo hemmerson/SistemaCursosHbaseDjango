57c75ee73d66dbd081ee2d8c657bace959923c05
Hemmerson Luis Barros da Rosa <hemmerson.rosa@gmail.com>
2024-09-04 12:05:07 -0300

    templates professor list_professor em views.py

--- sistemacursos/views.py
+++ sistemacursos/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from sistemacursos.models import Professor
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+# Views para Professor ==================================================================
+def list_professor(request):
+    professors = Professor.all()
+    return render(request, "professor/professor_list.html", {'professors': professors})
